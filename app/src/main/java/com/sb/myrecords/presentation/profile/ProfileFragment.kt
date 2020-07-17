29c162d3e21a2410c0f84c515e840cd963673af1
Francisco Sierra <fmsb0193@gmail.com>
2020-07-17 07:43:56 -0500

    Added a feature for pick a picture from gallery

--- app/src/main/java/com/sb/myrecords/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/sb/myrecords/presentation/profile/ProfileFragment.kt
@@ -1,6 +1,9 @@
 package com.sb.myrecords.presentation.profile
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.*
@@ -10,6 +13,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.snackbar.Snackbar
 import com.sb.myrecords.R
 import com.sb.myrecords.databinding.ProfileFragmentBinding
@@ -17,6 +21,7 @@ import com.sb.myrecords.di.Injectable
 import com.sb.myrecords.di.injectViewModel
 import com.sb.myrecords.ui.setExpandedToolbar
 import com.sb.myrecords.ui.setTitle
+import kotlinx.android.synthetic.main.picture_bottom_sheet_layout.view.*
 import javax.inject.Inject
 
 class ProfileFragment : Fragment(), Injectable {
@@ -28,7 +33,9 @@ class ProfileFragment : Fragment(), Injectable {
     private val args: ProfileFragmentArgs? by navArgs()
 
     private lateinit var binding: ProfileFragmentBinding
+    private lateinit var bottomSheet: BottomSheetDialog
 
+    @SuppressLint("InflateParams")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -44,11 +51,30 @@ class ProfileFragment : Fragment(), Injectable {
 
         subscribeUI()
 
+        bottomSheet = BottomSheetDialog(requireContext())
+        val bottomView = layoutInflater.inflate(R.layout.picture_bottom_sheet_layout, null)
+        bottomSheet.setContentView(bottomView)
+
+
         binding.changePhotoButton.setOnClickListener {
-            if (allPermissionsGranted()) {
-                it.findNavController().navigate(R.id.cameraFragment)
+            bottomSheet.show()
+        }
+
+        bottomView.take_picture_option.setOnClickListener {
+            if (allCameraPermissionsGranted()) {
+                binding.root.findNavController().navigate(R.id.cameraFragment)
+                bottomSheet.dismiss()
             } else {
-                requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+                requestPermissions(REQUIRED_CAMERA, REQUEST_CODE_CAMERA)
+            }
+        }
+
+        bottomView.open_gallery_option.setOnClickListener {
+            if (allExternalStoragePermissionsGranted()) {
+                openGallery()
+                bottomSheet.dismiss()
+            } else {
+                requestPermissions(REQUIRED_EXTERNAL_STORAGE, REQUEST_CODE_EXTERNAL_STORAGE)
             }
         }
 
@@ -59,15 +85,40 @@ class ProfileFragment : Fragment(), Injectable {
         requestCode: Int, permissions: Array<String>, grantResults:
         IntArray
     ) {
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
+        if (requestCode == REQUEST_CODE_CAMERA) {
+            if (allCameraPermissionsGranted()) {
                 binding.root.findNavController().navigate(R.id.cameraFragment)
+                bottomSheet.dismiss()
             } else {
                 Snackbar.make(
                     requireView(),
                     "Permissions not granted by the user.",
                     Snackbar.LENGTH_SHORT
                 ).show()
+            }
+        }
+
+        if (requestCode == REQUEST_CODE_EXTERNAL_STORAGE) {
+            if (allExternalStoragePermissionsGranted()) {
+                openGallery()
+                bottomSheet.dismiss()
+            } else {
+                Snackbar.make(
+                    requireView(),
+                    "Permissions not granted by the user.",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_EXTERNAL_STORAGE) {
+            viewModel.updatedUser.img = data?.data.toString()
+            binding.apply {
+                user = viewModel.updatedUser
+                executePendingBindings()
             }
         }
     }
@@ -110,17 +161,31 @@ class ProfileFragment : Fragment(), Injectable {
         })
     }
 
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+    private fun allCameraPermissionsGranted() = REQUIRED_CAMERA.all {
         activity?.baseContext?.let {context ->
             ContextCompat.checkSelfPermission(
                 context, it)
         } == PackageManager.PERMISSION_GRANTED
     }
 
+    private fun allExternalStoragePermissionsGranted() = REQUIRED_EXTERNAL_STORAGE.all {
+        activity?.baseContext?.let {context ->
+            ContextCompat.checkSelfPermission(
+                context, it)
+        } == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_CODE_EXTERNAL_STORAGE)
+    }
     //endregion
 
     companion object {
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_CAMERA = 10
+        private const val REQUEST_CODE_EXTERNAL_STORAGE = 100
+        private val REQUIRED_CAMERA = arrayOf(Manifest.permission.CAMERA)
+        private val REQUIRED_EXTERNAL_STORAGE = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
     }
 }
